c2ee4585eee31e6cde72e83a4ad0f170bba3d10e
ppppppsmash <pppppp.work@gmail.com>
2024-02-27 22:19:19 +0900

    feat: movie slug page

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -4,11 +4,13 @@
 
 <div>
   <div class="flex flex-col justify-around p-4">
-    <img
-      class="w-full h-[30vh] object-cover rounded-2xl mb-4"
-      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-      alt={movie.title}
-    />
+    <a href={`/movie/${movie.id}`}>
+      <img
+        class="w-full h-[30vh] object-cover rounded-2xl mb-4"
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+      />
+    </a>
 
     <div class="h-[5vh]">
       <h2 class="text-normal">{movie.title}</h2>
